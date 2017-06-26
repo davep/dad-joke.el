98e5e5ed4c57f6076afb55d75af66e787867d9f9
Dave Pearson <davep@davep.org>
2017-06-26 10:52:52 +0100

    v1.2

--- dad-joke.el
+++ dad-joke.el
@@ -2,7 +2,7 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.1
+;; Version: 1.2
 ;; Keywords: games
 ;; URL: https://github.com/davep/dad-joke.el
 ;; Package-Requires: ((emacs "24"))
